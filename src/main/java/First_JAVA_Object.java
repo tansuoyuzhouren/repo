c0d3c83d281ca53ade20677a0d964f2aa39796a6
root <3358817353@qq.com>
2024-11-05 14:43:34 +0800

    one commit

---
+++ src/main/java/First_JAVA_Object.java
@@ -0,0 +1,6 @@
+public class First_JAVA_Object {
+    public static void main(String[] args) {
+        System.out.println("This is a demo file");
+        System.out.println("hello world");
+    }
+}
